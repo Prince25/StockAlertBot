08aef10a1d73b7f57c69dd3581871ae3c79ee95a
Jeisonx3 <JeisonAlejoHD@gmail.com>
2021-01-08 22:14:45 -0500

    bug fix

--- writeToFile.js
+++ writeToFile.js
@@ -1,11 +1,12 @@
 import console from "console";
 import fs from "fs";
 import util from 'util'
+import moment from "moment";
 
 export default async function writeErrorToFile(name, error) {
     fs.writeFile('log' + name + '.log', util.inspect(error), function(e, result) {
         if(e) console.error('File write error: ', e);
     });
-    console.error('Unhandled error for ' + name + '. Written to log' + name + '.log')
+    console.error(moment().format('LTS') + ': Unhandled error for ' + name + '. Written to log' + name + '.log')
     console.error('This is usually not a problem but if this error appears frequently, please report the error (and the log) to GitHub.')
 }
