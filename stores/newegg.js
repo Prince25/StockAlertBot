0d61257fc081eced4189c62a95d8cb2608cf159d
Jeisonx3 <JeisonAlejoHD@gmail.com>
2021-01-10 20:58:37 -0500

    Add Newegg Combos

--- stores/newegg.js
+++ stores/newegg.js
@@ -12,7 +12,7 @@ import open from "open"
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
     let interval = {
         unit: 'seconds',    // seconds, m: minutes, h: hours
-        value: 5           
+        value: 5
     }
     let url = 'https://www.newegg.com/p/N82E16868110292'
     newegg(url, interval);
@@ -23,6 +23,50 @@ const store = 'Newegg'
 let firstRun = new Set();
 let urlOpened = false;
 export default async function newegg(url, interval) {
+    if (url.includes('ComboDealDetails')) neweggCombos(url, interval)
+    else {
+        try {
+            let res = await axios.get(url, {
+                headers: {
+                    'User-Agent': USER_AGENTS[Math.floor(Math.random() * USER_AGENTS.length)]
+                }
+            }).catch(async function (error) {
+                if (error.response.status == 503) console.error(moment().format('LTS') + ': ' + store + ' 503 (service unavailable) Error. Interval possibly too low. Consider increasing interval rate.')
+                else writeErrorToFile(store, error);
+            });
+
+            if (res && res.status == 200) {
+                let parser = new DomParser();
+                let doc = parser.parseFromString(res.data, 'text/html');
+                let title = doc.getElementsByClassName('product-title')[0].innerHTML.trim().slice(0, 150)
+                let inventory = doc.getElementsByClassName('btn btn-primary btn-wide')
+                let image = doc.getElementsByClassName('image_url')[0].textContent
+
+                if (inventory.length > 0) inventory = inventory[0].firstChild.textContent
+                if ((!inventory || inventory != 'Add to cart ') && !firstRun.has(url)) {
+                    console.info(moment().format('LTS') + ': "' + title + '" not in stock at ' + store + '.' + ' Will keep retrying in background every', interval.value, interval.unit)
+                    firstRun.add(url)
+                }
+                else if (inventory && inventory == 'Add to cart ') {
+                    if (ALARM) threeBeeps();
+                    if (OPEN_URL && !urlOpened) {
+                        open(url);
+                        sendAlertToWebhooks(url, title, image, store)
+                        urlOpened = true;
+                        setTimeout(() => urlOpened = false, 1000 * 295) // Open URL and post to webhook every 5 minutes
+                    }
+                    console.info(moment().format('LTS') + ': ***** In Stock at ' + store + ' *****: ', title);
+                    console.info(url);
+                }
+            }
+
+        } catch (e) {
+            writeErrorToFile(store, e)
+        }
+    }
+};
+
+async function neweggCombos(url, interval) {
     try {
         let res = await axios.get(url, {
             headers: {
@@ -36,21 +80,21 @@ export default async function newegg(url, interval) {
         if (res && res.status == 200) {
             let parser = new DomParser();
             let doc = parser.parseFromString(res.data, 'text/html');
-            let title = doc.getElementsByClassName('product-title')[0].innerHTML.trim().slice(0, 150)
-            let inventory = doc.getElementsByClassName('btn btn-primary btn-wide')
-            let image = doc.getElementsByClassName('image_url')[0].textContent
-            
+            let title = doc.getElementsByTagName('title')[0].textContent
+            let inventory = doc.getElementsByClassName('atnPrimary')
+            let image = 'https:' + doc.getElementById('mainSlide_0').getAttribute('src')
+
             if (inventory.length > 0) inventory = inventory[0].firstChild.textContent
-            if ((!inventory || inventory != 'Add to cart ') && !firstRun.has(url)) {
+            if ((!inventory || inventory != 'Add to Cart ') && !firstRun.has(url)) {
                 console.info(moment().format('LTS') + ': "' + title + '" not in stock at ' + store + '.' + ' Will keep retrying in background every', interval.value, interval.unit)
                 firstRun.add(url)
             }
-            else if (inventory && inventory == 'Add to cart ') {
+            else if (inventory && inventory == 'Add to Cart ') {
                 if (ALARM) threeBeeps();
-                if (OPEN_URL && !urlOpened) { 
-                    open(url); 
+                if (OPEN_URL && !urlOpened) {
+                    open(url);
                     sendAlertToWebhooks(url, title, image, store)
-                    urlOpened = true; 
+                    urlOpened = true;
                     setTimeout(() => urlOpened = false, 1000 * 295) // Open URL and post to webhook every 5 minutes
                 }
                 console.info(moment().format('LTS') + ': ***** In Stock at ' + store + ' *****: ', title);
