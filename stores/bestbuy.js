dc953418266023940aa98ce4a58cdc13e50ff31e
Prabhjot Singh <prince112592@yahoo.com>
2021-03-27 23:52:33 -0700

    Support Best Buy Canada

--- stores/bestbuy.js
+++ stores/bestbuy.js
@@ -7,7 +7,6 @@ import axios from "axios";
 import moment from "moment";
 import DomParser from "dom-parser";     // https://www.npmjs.com/package/dom-parser
 import open from "open"
-import console from "console";
 
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
@@ -37,26 +36,49 @@ export default async function bestbuy(url, interval) {
             if (error.response.status == 503) console.error(moment().format('LTS') + ': ' + store + ' 503 (service unavailable) Error. Interval possibly too low. Consider increasing interval rate.')
             else writeErrorToFile(store.replace(' ', ''), error);
         });
-        
+
         if (res && res.status === 200) {
             let parser = new DomParser();
             let doc, title, inventory, open_box, image
            
-            // Check package products
-            if (url.includes('combo')) {
-                doc = parser.parseFromString(res.data, 'text/html');
+            doc = parser.parseFromString(res.data, 'text/html');
+            if (url.includes('bestbuy.ca')) {   // Check Best buy Canada
+                let scripts = doc.getElementsByTagName('script')
+                if (scripts.length > 0) {
+                    let json  = scripts.filter(script => script.textContent.includes("window.__INITIAL_STATE__ "))
+                    json = json[0].textContent;
+                    if (json && json.length > 0) {
+                        json = json.slice(json.indexOf("window.__INITIAL_STATE__ = {") + 27, json.indexOf('};') + 1)
+                        try { 
+                            json = JSON.parse(json)
+                            let productInfo = json.product
+                            title = productInfo.product.name
+                            image = productInfo.product.productImage
+                            if (productInfo.availability.shipping.purchasable) inventory = "Add to Cart"
+                        } catch {}
+                    }
+                } else {
+                    inventory = doc.getElementsByClassName('addToCartButton')
+                    if (inventory.length > 0) inventory = inventory[0].getAttribute('disabled')
+                    if (inventory === null) inventory = "Add to Cart"
+                    try { title = doc.getElementsByTagName('title')[0].textContent.replace(" | Best Buy Canada", "") } catch {}
+                    try { image = doc.getElementsByTagName('link').filter(
+                        link => link.getAttribute('rel') && link.getAttribute('rel') == "preload" &&
+                        link.getAttribute('as') && link.getAttribute('as') == "image")[0].getAttribute("href")} catch {}
+                }
+            }
+            else if (url.includes('combo')) {    // Check package products
                 inventory = doc.getElementsByClassName('add-to-cart-button')
                 try { title = doc.getElementsByClassName('sku-title')[0].textContent } catch {}
                 try { image = doc.getElementsByClassName('picture-wrapper')[0].getElementsByTagName('img')[0].getAttribute('src') } catch {}
             } else { // Check normal products
-                doc = parser.parseFromString(res.data, 'text/html');
                 try { title = doc.getElementsByClassName('sku-title')[0].childNodes[0].textContent.trim().slice(0, 150) } catch {}
                 inventory = doc.getElementsByClassName('add-to-cart-button')
                 open_box = doc.getElementsByClassName('open-box-option__label')
                 try { image = doc.getElementsByClassName('primary-image')[0].getAttribute('src') } catch {}
             } 
 
-            if (inventory.length > 0) inventory = inventory[0].textContent
+            if (inventory && typeof(inventory) == "object" && inventory.length > 0) inventory = inventory[0].textContent
             if (open_box && open_box.length > 0) {
                 if (ALARM) threeBeeps();
                 if (!runtimeData[url]['urlOpened']) { 
