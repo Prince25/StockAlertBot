eaa6d7ecba532cf1172200249563ee83cebd1d7e
Prabhjot Singh <prince112592@yahoo.com>
2021-04-15 01:40:30 -0700

    Update Ebuyer to include Preorder

--- src/stores/ebuyer.js
+++ src/stores/ebuyer.js
@@ -4,14 +4,17 @@ export default function ebuyer(html) {
 	try {
 		const TITLE_SELECTOR = ".product-hero__title"
 		const IMAGE_SELECTOR = "div.image-gallery__hero > a > img"
-		const INVENTORY_SELECTOR = ".js-add-to-basket-main.js-show-loader"
+		const INVENTORY_SELECTOR = ".js-add-to-basket-main:first"
 
 		const $ = cheerio.load(html)
-		const title = $(TITLE_SELECTOR).text().trim()
+		let title = $(TITLE_SELECTOR).text().trim()
 		const image = $(IMAGE_SELECTOR).attr('src')
 		let inventory = $(INVENTORY_SELECTOR).attr('value')
 
 		if (inventory == 'Add to Basket') {
+			inventory = true
+		} else if (inventory == 'Pre-order') {	// Check for preorder
+			title += ' - Preorder'
 			inventory = true
 		} else {
 			inventory = false
