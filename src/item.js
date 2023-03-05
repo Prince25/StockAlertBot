664943d7f077dd544d3bd1e4a0cf8adb74e4c4ef
S <87047045+Sup3820@users.noreply.github.com>
2023-03-05 21:45:59 +0000

    code revision
    update code and optimize and fix small bugs

--- src/item.js
+++ src/item.js
@@ -2,66 +2,60 @@ import { toFile } from "./utils/log.js";
 import { fetchPage } from "./utils/fetch.js";
 
 export default class Item {
-	constructor(url) {
-		this.url = url;
-		this.notificationSent = false;
-		this.shouldSendNotification = true;
-		this.html = undefined;
-		this.info = {
-			title: undefined,
-			inventory: undefined,
-			image: undefined,
-		};
-	}
+  constructor(url) {
+    this.url = url;
+    this.notificationSent = false;
+    this.shouldSendNotification = true;
+    this.html = null;
+    this.info = {
+      title: null,
+      inventory: null,
+      image: null,
+    };
+  }
 
-	/*
-		Fetches the item page and assigns the html to this.html
-		Returns a promise of true if successful, false otherwise
-	*/
-	async getPage(store, use_proxies, badProxies) {
-		const response = await fetchPage(this.url, store, use_proxies, badProxies);
-		switch (response.status) {
-			case "ok":
-				this.html = response.html;
-				return {
-					status: "ok",
-				};
+  /**
+   * Fetches the item page and assigns the html to this.html
+   * Returns a promise of an object with status and badProxies keys
+   */
+  async getPage(store, useProxies, badProxies) {
+    const { status, html, error, badProxies: responseBadProxies } = await fetchPage(this.url, store, useProxies, badProxies);
+    
+    this.html = html;
+    if (status === "error") {
+      toFile(store, error, this);
+    }
+    
+    return { status, badProxies: responseBadProxies };
+  }
 
-			case "retry":
-				this.html = response.html;
-				return {
-					status: "retry",
-					bad_proxies: response.badProxies,
-				};
-
-			case "error":
-				this.html = response.html;
-				toFile(store, response.error, this);
-				return {
-					status: "error",
-				};
-		}
-	}
-
-	/*
-		Extract item information based on the passed callback function and assigns it to this.info
-		Returns true if successful, false otherwise
-	*/
-	async extractInformation(store, storeFunction) {
-		const info = await storeFunction(this.html);
-		if (info.title && info.image && typeof info.inventory == "boolean") {
-			// Change notification status to false once item goes out of stock
-			if (this.notificationSent && !info.inventory) this.notificationSent = false;
-
-			this.shouldSendNotification = !this.info.inventory && info.inventory; // Check change in item stock
-			this.info = info;
-			return true;
-		} else if (info.error) {
-			toFile(store, info.error, this);
-			return false;
-		} else {
-			toFile(store, "Unable to get information", Object.assign(this, info));
-			return false;
-		}
-	}
+  /**
+   * Extract item information based on the passed callback function and assigns it to this.info
+   * Returns a boolean indicating success or failure
+   */
+  async extractInformation(store, storeFunction) {
+    const info = await storeFunction(this.html);
+    
+    if (!info.title || !info.image || typeof info.inventory !== "boolean") {
+      toFile(store, "Unable to get information", Object.assign(this, info));
+      return false;
+    }
+    
+    const { title, image, inventory } = info;
+    const { inventory: currentInventory } = this.info;
+    
+    if (this.notificationSent && !inventory) {
+      this.notificationSent = false;
+    }
+    
+    this.shouldSendNotification = !currentInventory && inventory;
+    this.info = { title, image, inventory };
+    
+    if (info.error) {
+      toFile(store, info.error, this);
+      return false;
+    }
+    
+    return true;
+  }
 }
